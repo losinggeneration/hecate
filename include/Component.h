ca6fbf20428b7d526f3d4194d8122875cba6691b
Harley Laue <losinggeneration@gmail.com>
2012-05-10 23:12:00 -0500

    Make components have a string type

--- include/Component.h
+++ include/Component.h
@@ -1,9 +1,20 @@
 #ifndef COMPONENT_H
 #define COMPONENT_H
 
+#include <string>
+
 namespace hecate {
 
 class Component {
+public:
+	Component();
+	Component(std::string type);
+
+	void setType(std::string type);
+	std::string getType();
+
+private:
+	std::string type;
 };
 
 }
--- src/Component.cpp
+++ src/Component.cpp
@@ -1,2 +1,20 @@
 #include "Component.h"
 
+namespace hecate {
+
+Component::Component() {
+}
+
+Component::Component(std::string type) {
+	setType(type);
+}
+
+void Component::setType(std::string type) {
+	this->type = type;
+}
+
+std::string Component::getType() {
+	return type;
+}
+
+}
