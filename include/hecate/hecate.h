a61df59bae1271cd7d9bf5585de8e6f1b20716c8
Harley Laue <losinggeneration@gmail.com>
2012-05-18 14:49:43 -0500

    Add convience hecate.h file to include all - systems

---
+++ include/hecate/hecate.h
@@ -0,0 +1,18 @@
+#ifndef HECATE_HECATE_H
+#define HECATE_HECATE_H
+
+#include "Component.h"
+#include "ComponentMapper.h"
+#include "ComponentType.h"
+#include "ComponentTypeManager.h"
+#include "Entity.h"
+#include "EntityManager.h"
+#include "EntitySystem.h"
+#include "GroupManager.h"
+#include "SystemBitManager.h"
+#include "SystemManager.h"
+#include "TagManager.h"
+#include "Types.h"
+#include "World.h"
+
+#endif
